3b7e4fbe20e430fbc6aa390db61411967cf048f4
mattShanahan7 <mjs327@gmail.com>
2017-04-21 11:27:50 -0400

    changes

---
+++ main/java/com/example/matthew/chineseflashcards/Dictionary.java
@@ -0,0 +1,30 @@
+package com.example.matthew.chineseflashcards;
+
+/**
+ * Created by Matthew on 4/21/17.
+ */
+
+public final class Dictionary
+{
+    public static final String[] characters = {
+            "⼸", "⼳", "⽇", "⼼", "⼽", "⼿", "⽉", "⽊", "⽬",
+            "⽼", "⼈", "⼀", "⼆", "⼋", "⼗", "⼉", "⻔", "⼑",
+            "⼒", "⻢", "⼟", "⼣", "⼤", "⼥", "⼦", "⼨", "⼩"
+    };
+
+    public static final String[] pinyin = {
+            "gōng", "yāo", "gōng", "rì", "xīn", "gē", "shǒu", "yuè",
+            "mù", "mù", "lǎo", "rén", "yī", "èr", "bā", "shí", "ér",
+            "mén", "dāo", "lì", "mǎ", "tǔ", "xī", "dà", "nǔ", "zǐ",
+            "cùn", "xiào"
+    };
+
+    public static final String[] english = {
+            "bow", "tiny", "work", "sun", "heart", "dagger-axe", "hand",
+            "moon", "wood", "eye", "old", "person", "one", "two", "eight",
+            "ten", "child", "door", "knife", "power", "horse", "earth",
+            "sunset", "big", "female", "son", "inch", "small"
+    };
+
+
+}
